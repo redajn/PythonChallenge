21216fb5e6a355a979d005e353396bee40158523
redajn <redajn@yandex.ru>
2017-12-04 21:39:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,12 @@
 x = int(input("Insert: "))
 def calc(num):
-    if num % 3 == 0:
+    if  type(num) is not int:
+        print ("Is not number")
+    elif num % 3 == 0:
         print("True")
     else:
         print("False")
+ #   else:
+  #      print ("Incorrect type")
+   #     x = input("Insert: ")
 calc(x)
